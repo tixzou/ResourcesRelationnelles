b36c065dcdfe270ad9ef40deb7527470c256345f
schhean <sokchychhean66@gmail.com>
2026-03-05 08:47:48 +0100

    Init navbar front

--- Front/app/layout.tsx
+++ Front/app/layout.tsx
@@ -7,7 +7,7 @@ import { Providers } from "./providers";
 
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
-import { Navbar } from "@/components/navbar";
+import NavbarComponent from "@/components/navbar";
 
 export const metadata: Metadata = {
   title: {
@@ -43,20 +43,12 @@ export default function RootLayout({
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
-            <Navbar />
+            <NavbarComponent />
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
               {children}
             </main>
             <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://heroui.com?utm_source=next-app-template"
-                title="heroui.com homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">HeroUI</p>
-              </Link>
+             
             </footer>
           </div>
         </Providers>
