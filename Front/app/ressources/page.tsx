1a9c08a3187b123ce93511c5fc003e09d99c091d
schhean <sokchychhean66@gmail.com>
2026-03-05 10:01:24 +0100

    Création des modales d'inscription et de connexion.

---
+++ Front/app/ressources/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <h1> Partie ressources </h1>
+      </div>
+    </>
+  );
+}
--- Front/components/navbar.tsx
+++ Front/components/navbar.tsx
@@ -1,20 +1,10 @@
+"use client";
+
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/navbar";
 import { Button } from "@heroui/button";
 import { Link } from "@heroui/link";
 import Image from "next/image";
-
-export const AcmeLogo = () => {
-  return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
-  );
-};
+import AuthModal from "./authentification/authModal";
 
 export default function NavbarComponent() {
   return (
@@ -27,22 +17,22 @@ export default function NavbarComponent() {
           alt="Logo de (RE)Sources Relationnelles"
           className="w-10 h-auto object-contain opacity-80"
         />
+        <Link href="/">
         <p className="font-bold text-inherit">
           (RE)Sources <span className="text-[#003E7E]">Relationnelles</span>
         </p>
+        </Link>
       </NavbarBrand>
 
       <NavbarContent justify="end">
         <NavbarItem>
-          <Link color="foreground" href="#">
+          <Link color="foreground" href="/ressources" className="hover:text-[#003E7E]">
             Ressources
           </Link>
         </NavbarItem>
         <span className="text-gray-500">|</span>
         <NavbarItem>
-          <Button as={Link} className="hover:bg-[#003E7E] hover:text-white text-[#003E7E]" href="#" variant="flat">
-            Se connecter
-          </Button>
+          <AuthModal />
         </NavbarItem>
       </NavbarContent>
     </Navbar>
